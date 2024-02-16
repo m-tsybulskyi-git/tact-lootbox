0fa651fff6b9777ba6108533711b28c0a81bd0c8
Mykyta_Tsybulskyi <@kits.kfplc.com>
2024-02-16 14:31:36 +0200

    Remove output and upgrade contract

--- sources/contract.read.ts
+++ sources/contract.read.ts
@@ -1,15 +1,26 @@
-import { Address} from "@ton/core";
-import { TonClient4 } from "@ton/ton";
+import { getHttpEndpoint } from "@orbs-network/ton-access";
+import { TonClient } from "@ton/ton";
+import { Address, OpenedContract, fromNano  } from "@ton/core";
 import { LootBoxContract } from "./output/sample_LootBoxContract";
 
 (async () => {
-    const client = new TonClient4({
-        endpoint: "https://sandbox-v4.tonhubapi.com",
+    const client = new TonClient({
+        endpoint: await getHttpEndpoint({ network: "testnet" }),
     });
 
-    let contract_address = Address.parse("EQCHcWAcKtbY5E75piPJYmqyhKwDNa7fF5SU2LTzEWnzFL7-");
+    let contract_address = Address.parse("EQDYfALh5zjhkNDDIwyRXtTtQKLVcrNs2xiprpWmDyvdIU2c");
+
     let contract = LootBoxContract.fromAddress(contract_address);
-    let contract_open = client.open(contract);
-    let param = await contract_open.getBiggestWin() / BigInt(1000000000);
-    console.log("getBiggestWin: " + (param));
+    let contract_opened = client.open(contract) as OpenedContract<LootBoxContract>;
+
+    let address = Address.parse("0:65e8a3453e3cff10c7bee7834bc5932940cbb9f6afee2d72770f89150990cca2")
+    let paramUserStats = await contract_opened.getUserStats(address);
+    console.log("user stats: counter: " + (paramUserStats?.counter));
+    console.log("user stats: biggestWin: " + fromNano(paramUserStats?.biggestWin!!));
+    console.log("user stats: lastWin: " + fromNano(paramUserStats?.lastWin!!));
+
+    let paramGlobalStats = await contract_opened.getGlobalStats();
+    console.log("global stats: counter: " + (paramGlobalStats?.counter));
+    console.log("global stats: biggestWin: " + fromNano(paramGlobalStats?.biggestWin!!));
+    console.log("global stats: lastWin: " + fromNano(paramGlobalStats?.lastWin!!));
 })();
